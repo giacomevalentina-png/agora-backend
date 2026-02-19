a7f3401ffd015483b732f6e8c12f9dc86e1740a2
Valentina Ancona <vale@Valentinas-MacBook-Pro.local>
2026-02-19 14:36:06 +0900

    Fix CORS configuration

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 import json
+import os
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///agora.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
